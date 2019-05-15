4503d3e83415beb273f772125d86ecbd54b23cc5
Benjamin Kuschner <benjamin.kuschner@gmail.com>
2019-05-14 21:47:09 -0700

    Created User class. added hashing for passwords. Scrapped Serializeable for persistant storage

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/22/f018c3deca7600191b75c7b8dc14a2f8
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private String username;
+	private String password;
+	
+	/*
+	 * Creates a new user; expects valid password
+	 * @param uname username
+	 * @param pwd password
+	 * @return true if successful, false otherwise
+	 */
+	public User (String uname, String pwd) {
+		username = uname;
+		password = pwd;
+	}
+}
--- PasswordVerifier/src/User.java
+++ PasswordVerifier/src/User.java
@@ -1,8 +1,10 @@
-import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-public class User implements Serializable {
+public class User {
 	private String username;
-	private String password;
+	private byte[] passwordHash;
+	private static String hashFunc = "SHA-256";
 	
 	/*
 	 * Creates a new user; expects valid password
@@ -12,6 +14,44 @@ public class User implements Serializable {
 	 */
 	public User (String uname, String pwd) {
 		username = uname;
-		password = pwd;
+		MessageDigest myDigest;
+		try {
+			myDigest = MessageDigest.getInstance(hashFunc);
+			passwordHash = myDigest.digest(pwd.getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/*
+	 * checks if an attempted username matches the true username
+	 * @param uname attempted username
+	 * @return true if match false otherwise
+	 */
+	public boolean checkUname (String uname) {
+		if(uname.equals(username))
+			return true;
+		return false;
+	}
+	
+	/*
+	 * checks if an attempted password matches the true password
+	 * @param uname attempted password
+	 * @return true if match false otherwise
+	 */
+	public boolean checkPwd (String pwd) {
+		MessageDigest myDigest;
+		try {
+			myDigest = MessageDigest.getInstance(hashFunc);
+			byte[] attemptHash = myDigest.digest(pwd.getBytes());
+			if(MessageDigest.isEqual(attemptHash, passwordHash))
+				return true;
+			else
+				return false;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
